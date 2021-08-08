a171dbea55861aba26413754fe0915d18b1e8215
Max Belkin <bernardito.luis.el.gorra@gmail.com>
2021-08-08 15:00:53 +0300

    Always scroll to top when page changed.

--- frontend/static/index.js
+++ frontend/static/index.js
@@ -366,7 +366,10 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
 
 const app = new Vue({
